169818ab4cd944ace6b578b46d36c203c44e5c35
Henrique Mateus Vargas dos Santos <108635477+DevHenriquecido@users.noreply.github.com>
2022-09-03 18:01:57 -0300

    Update NumeroMagico.js

--- NumeroMagico.js
+++ NumeroMagico.js
@@ -1,47 +1,49 @@
-let numero = []
-let sorteio = []
+let numero = [] //Vetor do número que será comparado
+let sorteio = [] //Vetor onde será guardado o número sorteado
 let tentativasMagicoFacil = 1
 let tentativasMagicoMedio = 1
 let tentativasMagicoDificil = 1
 
 
-document.getElementById("divMagicoModoFacil").style.display = 'none'
-document.getElementById("divMagicoModoMedio").style.display = 'none'
-document.getElementById("divMagicoModoDificil").style.display = 'none'
-document.getElementById("divPainel").style.display = 'none'
-document.getElementById("divVoltar").style.display = 'none'
-
+//Ir para tela do modo fácil
 function facilMagico(){
     document.getElementById("divMagicoModoFacil").style.display = 'inline'
     document.getElementById("divPainel").style.display = 'inline'
     document.getElementById("divVoltar").style.display = 'inline'
-    document.getElementById("divMagicoModoMedio").style.display = 'none'
-    document.getElementById("divMagicoModoDificil").style.display = 'none'
     document.getElementById("divJogos").style.display = 'none'
     document.getElementById("inpNumeroMagicoFacil").focus()
     sorteio = Math.round(Math.random() * 10)
-    
 }
 
+//Botão de tentativa do modo fácil
 function tenteMagicoFacil(){
+    //Pega o número do input que será comparado
     numero = document.getElementById("inpNumeroMagicoFacil").value
     
+    //Compara com o vetor do sorteio
     if (numero < sorteio) {
         document.getElementById("inpNumeroMagicoFacil").focus()
-        document.getElementById('divPainel').innerHTML = 'O número sorteado é maior' + '<br>' +
-        'Quantidade de tentativas: ' + tentativasMagicoFacil++
+        document.getElementById('divPainel').innerHTML = `O número sorteado é maior ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoFacil++}`
     }
         if (numero > sorteio) {
             document.getElementById("inpNumeroMagicoFacil").focus()
-            document.getElementById('divPainel').innerHTML = 'O número sorteado é menor' + '<br>' + 
-            'Quantidade de tentativas: ' + tentativasMagicoFacil++ 
+            document.getElementById('divPainel').innerHTML = `O número sorteado é menor ${'<br>'}
+            Quantidade de tentativas:  ${tentativasMagicoFacil++}`
         }
+        if (numero == sorteio) {
+            document.getElementById("inpNumeroMagicoFacil").focus()
+            document.getElementById('botaoMagicoFacil').disabled = true
+            document.getElementById('divPainel').innerHTML = `Acertou! ${'<br>'}
+            Quantidade de tentativas:  ${tentativasMagicoFacil}` 
+            }
 
     if (tentativasMagicoFacil >= 4){
-        document.getElementById('divPainel').innerHTML = 'Acabou as chances do nível Fácil, o número era: ' + sorteio      
+        document.getElementById("inpNumeroMagicoFacil").value = ''
+        document.getElementById('divPainel').innerHTML = `Acabou as chances do nível Fácil, o número era: ${sorteio}`      
      } 
     }
-    
+    // Botão de reiniciar o jogo do modo fácil
     function tenteDeNovoMagicoFacil(){   
         sorteio = Math.round(Math.random() * 10)
         tentativasMagicoFacil = 1
@@ -49,42 +51,43 @@ function tenteMagicoFacil(){
         document.getElementById("inpNumeroMagicoFacil").focus()
         document.getElementById("inpNumeroMagicoFacil").value = ''
     }
-    
+    //Ir para tela do modo médio
     function medioMagico() {
     document.getElementById("divMagicoModoMedio").style.display = 'inline'
     document.getElementById("divPainel").style.display = 'inline'
     document.getElementById("divVoltar").style.display = 'inline'
-    document.getElementById("divMagicoModoDificil").style.display = 'none'
-    document.getElementById("divMagicoModoFacil").style.display = 'none'
     document.getElementById("divJogos").style.display = 'none'
     document.getElementById("inpNumeroMagicoMedio").focus()
     sorteio = Math.round(Math.random() * 50)
 }
 
+//Botão de tentativa do modo médio
 function tenteMagicoMedio(){
+     //Pega o número do input que será comparado
     numero = document.getElementById("inpNumeroMagicoMedio").value
     
+    //Compara com o vetor do sorteio
     if (numero < sorteio) {
         document.getElementById("inpNumeroMagicoMedio").focus()
-        document.getElementById('divPainel').innerHTML = 'O número sorteado é maior' + '<br>' +
-        'Quantidade de tentativas: ' + tentativasMagicoMedio++
+        document.getElementById('divPainel').innerHTML = `O número sorteado é maior ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoMedio++}`
 }
         if (numero > sorteio) {
         document.getElementById("inpNumeroMagicoMedio").focus()
-        document.getElementById('divPainel').innerHTML = 'O número sorteado é menor' + '<br>' + 
-        'Quantidade de tentativas: ' + tentativasMagicoMedio++
+        document.getElementById('divPainel').innerHTML = `O número sorteado é menor ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoMedio++}`
 }
      if (numero == sorteio) {
         document.getElementById("inpNumeroMagicoMedio").focus()
-        document.getElementById('divPainel').innerHTML = 'Acertou!'+ '<br>' + 
-        'Quantidade de tentativas: ' + tentativasMagicoMedio 
+        document.getElementById('divPainel').innerHTML = `Acertou! ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoMedio}`
         }
      if (tentativasMagicoMedio >= 6){
         document.getElementById("inpNumeroMagicoMedio").value = ''
-        document.getElementById('divPainel').innerHTML = 'Acabou as chances do nível Médio, o número era: ' + sorteio  
+        document.getElementById('divPainel').innerHTML = `Acabou as chances do nível Médio, o número era: ${sorteio}`  
 }
 }
-
+   // Botão de reiniciar o jogo do modo médio
 function tenteDeNovoMagicoMedio(){
     sorteio = Math.round(Math.random() * 50)
     tentativasMagicoMedio = 1
@@ -93,42 +96,43 @@ function tenteDeNovoMagicoMedio(){
     document.getElementById("inpNumeroMagicoMedio").value = ''
 }
 
+//Ir para a tela do modo Difícil
 function dificilMagico() {
     document.getElementById("divMagicoModoDificil").style.display = 'inline'
     document.getElementById("divPainel").style.display = 'inline'
     document.getElementById("divVoltar").style.display = 'inline'
-    document.getElementById("divMagicoModoMedio").style.display = 'none'
-    document.getElementById("divMagicoModoFacil").style.display = 'none'
     document.getElementById("divJogos").style.display = 'none'
     document.getElementById("inpNumeroMagicoDificil").focus()
 
     sorteio = Math.round(Math.random() * 100)
 }
-
+//Botão de tentativa do modo difícil
 function tenteMagicoDificil(){
+    //Pega o número do input que será comparado
     numero = document.getElementById("inpNumeroMagicoDificil").value
 
+    //Compara com o vetor do sorteio
     if (numero < sorteio) {
         document.getElementById("inpNumeroMagicoDificil").focus()
-        document.getElementById('divPainel').innerHTML = 'O número sorteado é maior' + '<br>' +
-        'Quantidade de tentativas: ' + tentativasMagicoDificil++
+        document.getElementById('divPainel').innerHTML = `O número sorteado é maior ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoDificil++}`
     }
      if (numero > sorteio) {
         document.getElementById("inpNumeroMagicoDificil").focus()
-        document.getElementById('divPainel').innerHTML = 'O número sorteado é menor' + '<br>' + 
-        'Quantidade de tentativas: ' + tentativasMagicoDificil++
+        document.getElementById('divPainel').innerHTML = `O número sorteado é menor ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoDificil++}`
     }
      if (numero == sorteio) {
-        document.getElementById('divPainel').innerHTML = 'Acertou!'+ '<br>' + 
-        'Quantidade de tentativas: ' + tentativasMagicoDificil
+        document.getElementById('divPainel').innerHTML = `Acertou! ${'<br>'}
+        Quantidade de tentativas:  ${tentativasMagicoDificil}`
     }
      if (tentativasMagicoDificil >= 11){
         document.getElementById("inpNumeroMagicoDificil").value = ''
-        document.getElementById('divPainel').innerHTML = 'Acabou as chances do nível Difícil, o número era: ' + sorteio
+        document.getElementById('divPainel').innerHTML = `Acabou as chances do nível Médio, o número era: ${sorteio}`  
 
 }
 }
-
+ // Botão de reiniciar o jogo do modo Dificl
 function tenteDeNovoMagicoDificil(){
     sorteio = Math.round(Math.random() * 100)
     tentativasMagicoDificil = 1
@@ -137,6 +141,7 @@ function tenteDeNovoMagicoDificil(){
     document.getElementById("inpNumeroMagicoDificil").value = ''
 }
 
+//Recarrega a página
 function Volte(){
     location.reload()
 }
